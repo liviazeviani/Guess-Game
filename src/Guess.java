1ec31552abd131249ee7af3a122cba46b1a06bbc
liviapz <liviapavinizeviani@gmail.com>
2023-01-19 01:17:58 -0300

    Include a do, so when the user guess the number the code asks if the user wants to play again

--- src/Guess.java
+++ src/Guess.java
@@ -4,22 +4,30 @@ public class Guess {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        String playAgain;
 
-        int guessNumber = (int)(Math.random()* 101);
-        int theNumber = 0;
+        do {
+            int theNumber = (int) (Math.random() * 101);
+            int guessNumber = 0;
 
-        while(guessNumber != theNumber){
-            System.out.println("Please, guess the number i'm thinking (it is between 0 to 100)" +
-                    ": ");
-            theNumber = scanner.nextInt();
+            while (theNumber != guessNumber) {
+                System.out.println("Please, guess the number i'm thinking (it is between 0 to 100)" +
+                        ": ");
+                guessNumber = scanner.nextInt();
 
-            if(theNumber > guessNumber)
-                System.out.println("Humm, " + theNumber + " is too high, Try again: ");
-            else if(theNumber < guessNumber)
-                System.out.println("Humm, " + theNumber + " is too low, Try again: ");
-            else
-                System.out.println("Yay, " + guessNumber + " is the right number! ");
-        }
+                if (guessNumber > theNumber)
+                    System.out.println("Humm, " + guessNumber + " is too high, Try again: ");
+                else if (guessNumber < theNumber)
+                    System.out.println("Humm, " + guessNumber + " is too low, Try again: ");
+                else
+                    System.out.println("Yay, " + theNumber + " is the right number! ");
+            }
+            System.out.println("Do you wanna play again? (y/n): ");
+            playAgain = scanner.next();
+
+        } while (playAgain.equalsIgnoreCase("y"));
+        System.out.println("Allright! It was a good game. Bye!");
+        scanner.close();
 
     }
 }
