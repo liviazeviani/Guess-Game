cef0c2670eeff73b0fb2589ee7c947157f668554
liviapz <liviapavinizeviani@gmail.com>
2023-01-19 00:03:47 -0300

    Inside the lopp i put a if else condition, so the user will have a hint in wich range the number is

--- src/Guess.java
+++ src/Guess.java
@@ -8,10 +8,17 @@ public class Guess {
         int guessNumber = (int)(Math.random()* 101);
         int theNumber = 0;
 
-
         while(guessNumber != theNumber){
-            System.out.println("Please, guess the number i'm thinking: ");
+            System.out.println("Please, guess the number i'm thinking (it is between 0 to 100)" +
+                    ": ");
             theNumber = scanner.nextInt();
+
+            if(theNumber > guessNumber)
+                System.out.println("Humm, " + theNumber + " is too high, Try again: ");
+            else if(theNumber < guessNumber)
+                System.out.println("Humm, " + theNumber + " is too low, Try again: ");
+            else
+                System.out.println("Yay, " + guessNumber + " is the right number! ");
         }
 
     }
